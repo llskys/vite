f7e2cb0b9e5d4ccb501c4195fde92926807ceaab
dong_yang <dongyang@yeahzee.com>
2021-05-24 09:53:35 +0800

    update 竞态demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from 'react'
 // import RecoilDemo from './recoil'
-import { ContextTest, Geti18n, LifeCycleContainer, PersonalInfoComponent, SetState } from './react-demo'
-import EventTest from './event/event'
-// import './App.css'
-import MyPromise from './utils/myPromise'
-import { debounce, throttle } from './utils/util' 
+// import { ContextTest, Geti18n, LifeCycleContainer, PersonalInfoComponent, SetState } from './react-demo'
+// import EventTest from './event'
+import RaceCondition from './RaceConditions'
+// import MyPromise from './utils/myPromise'
+// import { debounce, throttle } from './utils/util' 
 
 function App() {
-  const obj = {
-    name: 'name1',
-    logName: function(){
-      console.log('111',this)
-    }
-  }
+  // const obj = {
+  //   name: 'name1',
+  //   logName: function(){
+  //     console.log('111',this)
+  //   }
+  // }
   // const testMyPromise = () =>{
   //   const p1 = new MyPromise((resolve, reject) => {
   //     setTimeout(() => {
@@ -29,7 +29,7 @@ function App() {
       {/* <button onClick={throttle(obj.logName, 1000, obj)}>TEST</button>
       <button onClick={debounce(obj.logName, 1000, obj)}>TEST1</button> */}
       {/* <ContextTest /> */}
-      <EventTest />
+      <RaceCondition />
     </>
   )
 }
---
+++ src/RaceConditions/index.jsx
@@ -0,0 +1,40 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-18 15:11:48
+ * @LastEditTime: 2021-05-24 09:37:43
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \coded:\vite-study\src\event\event.js
+ */
+import React, { useState, useEffect } from 'react'
+
+const delay = id => {
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve(`finish${id}`)
+        }, id%2===0? 500: 2000)
+    })
+}
+
+function RaceCondition() {
+    const [id, setId] = useState(0)
+    
+    useEffect(()=>{
+        // 解决因为入参发生变化导致多次请求，并且处理了请求顺序异常的问题
+        let didCancel = false
+        delay(id).then(data=>{
+            if (!didCancel) {
+                console.log(data);
+            }
+        });
+        return ()=>{
+            didCancel = true
+        }
+    },[id])
+    
+    return (
+        <button onClick={()=>setId(c=>c+1)}>test</button>
+    )
+  }
+  
+  export default RaceCondition
